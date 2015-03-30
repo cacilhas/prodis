ff236ba2518c761e631f73f51cd0fe9152ee04a4
Cacilhας, La Batalema <batalema@cacilhas.info>
2015-03-30 00:38:20 -0300

    more elegant logging code

--- logging.pl
+++ logging.pl
@@ -1,6 +1,6 @@
 ﻿% -*- Prolog -*-
 % @copyright ©2015, Rodrigo Cacilhας <batalema@cacilhas.info>
-:- module(logging, [log/3, set_logfile/1]).
+:- module(logging, [log/3, set_logfile/1, set_log_formatter/1]).
 :- [library(settings)].
 
 :- dynamic formatter/1.
@@ -23,27 +23,32 @@ set_logfile(LogFile) :-
     tell(Stream).
 
 
+set_log_formatter(Formatter) :-
+    retractall(formatter(_)),
+    asserta(formatter(Formatter)).
+
+
+valid_levels([debug, info, warn, error, fatal]).
+
+
 rlog(Level, Format, Args) :-
     setting(prodis:loglevel, MinLog),
-    level(Level, LevelNumber),
-    level(MinLog, Min),
-    LevelNumber >= Min,
+	valid_levels(Levels),
+	valid_log(Level, MinLog, Levels),
     formatter(Formatter),
     use_module(Formatter),
     Formatter:format_log(Level, Format, Args, Log),
     call(Log).
 
 
-set_formatter(Formatter) :-
-    retractall(formatter(_)),
-    asserta(formatter(Formatter)).
+valid_log(_Current, _Min, []) :- !, fail.
 
+valid_log(_Current, Min, [Min |_]) :- !.
 
-level(debug, 10).
-level(info, 20).
-level(warn, 30).
-level(error, 40).
-level(fatal, 50).
+valid_log(Current, _Min, [Current |_]) :- !, fail.
+
+valid_log(Current, Min, [_|Rest]) :- valid_log(Current, Min, Rest).
+
 
 formatter(log_formatter).
 
