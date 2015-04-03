30a7ed45db344210499a7fa039fe5a46fe5e9b24
Cacilhας, La Batalema <batalema@cacilhas.info>
2015-04-03 13:36:35 -0300

    test operators #WIP

---
+++ tests/test_operatiors.pl
@@ -0,0 +1,17 @@
+﻿% -*- Prolog -*-
+% @copyright ©2015, Rodrigo Cacilhας <batalema@cacilhas.info>
+:- module(test_operatiors, []).
+:- ['../src/operators'].
+
+:- begin_tests(operators).
+
+test(slice, forall(member(Args, [[[], 4, []],
+                                   [[1, 2, 3], 0, []],
+                                   [[1, 2, 3], 2, [1, 2]]]))) :-
+    apply(slice, Args).
+
+% TODO: test other predicates
+
+:- end_tests(operators).
+
+% vim:set et:syntax=prolog
